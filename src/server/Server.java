46fd2cb38c0a40943a5ba1c306660e2a237a750e
Vaibhav Misra <66348531+misraVaibhav@users.noreply.github.com>
2021-05-19 16:17:53 +0530

    Update Server.java
    Now the Server GUI shows clients joining and leaving as it happens and also any particular port to use can be specified by the server user.

--- src/server/Server.java
+++ src/server/Server.java
@@ -24,90 +24,127 @@ public class Server {
 	  * main method of the Main-Class of the jar file
 	  */
 	public static void main(String[] args) {
-		try {
-			new Server(new ServerSocket(0));
-		} catch (IOException ioe) {
-			System.out.println("Error in finding port to listen through: " + ioe.getMessage());
-		}
+		new Server();	
 	}
 	
 	/*
 	 * start Listening through any available port, generate GUI to inform the user of server running
 	 * and start listening through that port
 	 */
-	Server(ServerSocket server) {
-		this.server = server;
-		try {			
-			
-			clients = new Hashtable<String,Streams>();
-			
-			storePort();			
-			
-			setGUI();
-					
-			startListening();
-			
-		} catch(IOException ioe) {
-			System.out.println("Error during listening for clients: " + ioe.getMessage());
-		}
+	Server() {
+		
+		clients = new Hashtable<String,Streams>();
+		String message = "Using port number 50000\nTo listen to clients through a different port, type the port number:\n";
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {						
+				String str = (String)JOptionPane.showInputDialog(frame, message, "50000");
+				if(str == null) System.exit(0);			
+				str = str.trim();
+				pNumber = Integer.parseInt(str);
+//				new SwingWorker<Object, Object>() {
+//
+//					@Override
+//					protected Object doInBackground() throws Exception {
+						try {
+							Server.this.server = new ServerSocket(pNumber);
+						} catch(IOException ioe) {
+							System.out.println("Could't connect to server: " + ioe.getMessage());
+						}
+//						return null;
+//					}
+//
+//					@Override
+//					protected void done() {	
+						if(server == null) {							
+							JOptionPane.showMessageDialog(frame, "Port in Use!", "Error!" , JOptionPane.ERROR_MESSAGE);
+//							new Server();
+							System.exit(0);
+						}
+//						} else {						
+							setGUI();
+////						}
+////						super.done();
+////					}				
+////					
+////				}.execute();	
+//				
+//			}
+//		});	
+			}
+		});
 		
 	}
-	
-	/*
-	 * store port that is listening to clients in port.txt to inform clients of the port they need to connect to
-	 */
-	private void storePort() throws FileNotFoundException, IOException {
-		File fl = new File("port.txt");
-		FileOutputStream f = new FileOutputStream(fl,false);
-		f.write(String.valueOf(server.getLocalPort()).getBytes());
-		f.close();
-	}
-	
+			
+
 	/*
 	 * the following method code runs in Event Dispatch Thread as it conains Swing components 
 	 */	
 	private void setGUI() {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame.setDefaultLookAndFeelDecorated(true);
-				frame = new JFrame();
-				frame.add(new JLabel("Server Running..."));
-				frame.setPreferredSize(new Dimension(130,80));
-				frame.setResizable(false);
-				frame.pack();
-				frame.setVisible(true);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			}
-		});	
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		frame = new JFrame();
+		try {
+			sLabel = new JTextArea();
+			sLabelPane = new JScrollPane();
+			sLabelPane.setViewportView(sLabel);
+			sLabel.setEditable(false);
+			sLabel.setText("Server Listening at: \n" + InetAddress.getLocalHost() + "\nPort: " + pNumber + "\n\nIf client is on the same machine,\nuse localhost as the IP address\nand port as mentioned above,\nelse if on a different machine,\nuse the above IP address and port number.");
+		} catch (UnknownHostException uhe) {
+			uhe.printStackTrace();
+		}
+		frame.add(sLabelPane);
+		frame.setPreferredSize(new Dimension(255,200));
+		frame.setResizable(false);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		startListening();
 	}
 	
 	/*
 	 * start listening to specified port forever
 	 */	
-	private void startListening() throws IOException {
-		while(true) {			
-			Socket socket = server.accept();
-			
-			// after accepting a client, its respective client handler starts in a new thread
-			new Thread(new ClientHandler(socket)).start();
-			System.out.println("Client connected at: " + socket.getRemoteSocketAddress());
-		}
+	private void startListening() {
+		
+		new SwingWorker<Object,Object>() {
+
+			@Override
+			protected Object doInBackground() throws Exception {
+				while(true) {			
+					try {
+						Socket socket = server.accept();
+						new Thread(new ClientHandler(Server.this, socket)).start();
+						System.out.println("Client connected at: " + socket.getRemoteSocketAddress());
+					} catch(IOException ioe) {
+						System.out.println("Error establishing connection: " + ioe.getMessage());
+					}	
+					
+				}	
+			}
+		}.execute();
+		
 	}
 	
 	
-	public static Hashtable<String,Streams> getClients() {
-		return Server.clients;
+	public Hashtable<String,Streams> getClients() {
+		return clients;
 	}	
+	
+	public JTextArea getsLabel() {
+		return sLabel;
+	}
 	
 	/*
 	 * global variables 
 	 */
 	private ServerSocket server;	
-	JFrame frame;
+	private JFrame frame;
+	private JTextArea sLabel;
+	private JScrollPane sLabelPane;
+	private int pNumber;	
 	
 	/* 
 	 * hastable to store names and Input/Output streams of all the clients
 	 */
-	private static Hashtable<String,Streams> clients = new Hashtable<String,Streams>();
+	private Hashtable<String,Streams> clients = new Hashtable<String,Streams>();
 	
 }
