fdf68f7b67da141c1d565a8e00bb4f4184fb9c19
Vaibhav Misra <66348531+misraVaibhav@users.noreply.github.com>
2021-05-19 16:17:02 +0530

    Update ClientHandler.java
    Now the Server GUI shows clients joining and leaving as it happens and also any particular port to use can be specified by the server user.

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -21,9 +21,12 @@ public class ClientHandler implements Runnable {
 	/*
 	 * Constructor : store the input/output streams of the corresponding client's socket 
 	 */
-	ClientHandler(Socket socket) {
+	ClientHandler(Server server, Socket socket) {
 		try {
+			this.server = server;
+			this.socket = socket;
 			this.out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
 			this.in = new ObjectInputStream(socket.getInputStream());
 		} catch (SocketException se) {
 			System.out.println("Error establishing connection: " + se.getMessage());
@@ -36,6 +39,7 @@ public class ClientHandler implements Runnable {
 	 * the following method runs after the constructor's initialisation owing to the call to Thread's start method
 	 */
 	public void run() {
+		System.out.println("in thread");
 		try {
 			
 			 /*
@@ -49,16 +53,17 @@ public class ClientHandler implements Runnable {
 			/*
 			 * store client's name
 			 */
-			Server.getClients().put(name,new Streams(in,out));
-			System.out.println(str + " joined!");
-			
+			server.getClients().put(name,new Streams(in,out));
+            System.out.println(name + " at " + socket.getInetAddress().getHostAddress()+" joined the Chat!");	
+            server.getsLabel().setText(server.getsLabel().getText() + "\n\n" + name + " at " + socket.getRemoteSocketAddress()+" joined the Chat!");
+            
 			/*
 			 * notify all the clients of new client coming online
 			 */
-			Server.getClients().forEach((k,v) -> {
+			server.getClients().forEach((k,v) -> {
 				if(!k.equals(name)) {
 					try {
-						v.getOS().writeObject(new Message(new ArrayList<String>(Server.getClients().keySet())));
+						v.getOS().writeObject(new Message(new ArrayList<String>(server.getClients().keySet())));
 						v.getOS().flush();
 					} catch (IOException ioe) {
 						System.out.println("Error establishing connection: " + ioe.getMessage());
@@ -81,13 +86,13 @@ public class ClientHandler implements Runnable {
 					/*
 					 * when client requests for list of online clients
 					 */
-					out.writeObject(new Message(new ArrayList<String>(Server.getClients().keySet())));
+					out.writeObject(new Message(new ArrayList<String>(server.getClients().keySet())));
 					out.flush();
 				} else if(msg.getmType() == MessageType.CLIENT_GLOBAL_MESSAGE) {
 					/*
 					 * when client sends a global message
 					 */
-					Server.getClients().forEach((k,v) -> {						
+					server.getClients().forEach((k,v) -> {						
 						try {
 							v.getOS().writeObject(new Message(msg.getMessage(), this.name, MessageType.SERVER_GLOBAL_MESSAGE));
 							v.getOS().flush();
@@ -99,7 +104,7 @@ public class ClientHandler implements Runnable {
 					/*
 					 * when client sends a private message
 					 */
-					ObjectOutputStream out_ = Server.getClients().get(message.getPerson()).getOS(); 
+					ObjectOutputStream out_ = server.getClients().get(message.getPerson()).getOS(); 
 					out_.writeObject(new Message(message.getMessage(), this.name, MessageType.SERVER_PRIVATE_MESSAGE));
 					out_.flush();
 				}
@@ -108,9 +113,10 @@ public class ClientHandler implements Runnable {
 		} catch (ClassNotFoundException cnfe) {
 			System.out.println("Error establishing connection: " + cnfe.getMessage());
 		} catch (SocketException se) {
-			System.out.println(this.name + " left!");
-			Server.getClients().remove(name);
-			Server.getClients().forEach((k,v) -> {
+            System.out.println(name + " at " + socket.getInetAddress().getHostAddress()+" left the Chat!");		
+            server.getsLabel().setText(server.getsLabel().getText() + "\n\n" + name + " at " + socket.getRemoteSocketAddress()+" left the Chat!");
+            server.getClients().remove(name);
+			server.getClients().forEach((k,v) -> {
 				try {
 					v.getOS().writeObject(new Message(name, MessageType.SEND_CLIENT_LIST_LEFT) );
 					v.getOS().flush();
@@ -129,4 +135,6 @@ public class ClientHandler implements Runnable {
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 	private String name;
+	private Socket socket;
+	private Server server;
 }
